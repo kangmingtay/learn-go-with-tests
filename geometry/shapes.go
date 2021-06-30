1bc4fbe08e554fc3cbc1310e6ba495d96bc86a4b
kangmingtay <kang.ming1996@gmail.com>
2021-06-30 14:08:26 +0800

    interface

--- geometry/shapes.go
+++ geometry/shapes.go
@@ -1,5 +1,11 @@
 package shapes
 
+import "math"
+
+type Shape interface {
+	Area() float64
+}
+
 type Rectangle struct {
 	Width  float64
 	Height float64
@@ -9,6 +15,23 @@ func Perimeter(rectangle Rectangle) float64 {
 	return 2 * (rectangle.Width + rectangle.Height)
 }
 
-func Area(rectangle Rectangle) float64 {
-	return rectangle.Width * rectangle.Height
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func (t Triangle) Area() float64 {
+	return 0.5 * t.Base * t.Height
 }
--- geometry/shapes_test.go
+++ geometry/shapes_test.go
@@ -13,11 +13,41 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	rectangle := Rectangle{12, 6}
-	got := Area(rectangle)
-	var want float64 = 72
 
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
+	areaTests := []struct {
+		name    string
+		shape   Shape
+		hasArea float64
+	}{
+		{name: "Rectangle", shape: Rectangle{Width: 12, Height: 6}, hasArea: 72.0},
+		{name: "Circle", shape: Circle{Radius: 10}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, hasArea: 36.0},
 	}
+
+	for _, tt := range areaTests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.hasArea {
+				t.Errorf("%#v got %g want %g", tt.shape, got, tt.hasArea)
+			}
+		})
+	}
+
+	// checkArea := func(t testing.TB, shape Shape, want float64) {
+	// 	t.Helper()
+	// 	got := shape.Area()
+	// 	if got != want {
+	// 		t.Errorf("got %g want %g", got, want)
+	// 	}
+	// }
+
+	// t.Run("rectangles", func(t *testing.T) {
+	// 	rectangle := Rectangle{12, 6}
+	// 	checkArea(t, rectangle, 72.0)
+	// })
+
+	// t.Run("circles", func(t *testing.T) {
+	// 	circle := Circle{10}
+	// 	checkArea(t, circle, 314.1592653589793)
+	// })
 }
